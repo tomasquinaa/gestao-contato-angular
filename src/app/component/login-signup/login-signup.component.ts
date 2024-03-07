76873203d112098003921126ed94cb1c03ab7d80
Armando Quinanga <armando.quinanga@rcsangola.com>
2024-03-07 16:08:49 +0100

    feat: atualizado o login e o register

--- src/app/component/login-signup/login-signup.component.ts
+++ src/app/component/login-signup/login-signup.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { signup } from '../contactmodel';
 
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
   styleUrls: ['./login-signup.component.css'],
 })
-export class LoginSignupComponent {
+export class LoginSignupComponent implements OnInit {
   isshow = false;
+  signupform!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.signupform = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   signup() {
     this.isshow = true;
@@ -14,5 +28,14 @@ export class LoginSignupComponent {
 
   login() {
     this.isshow = false;
+  }
+
+  submitsignup() {
+    this.http
+      .post<signup>('http://localhost:3000/signup', this.signupform.value)
+      .subscribe((res) => {
+        alert('user sucesso!');
+        this.signupform.reset();
+      });
   }
 }
